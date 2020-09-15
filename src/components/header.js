750e1ed560e59944dc6c70b2c93bb6d4fb8cda97
ren1el <renielxocampo@gmail.com>
2020-09-14 21:53:40 -0700

    Created header and layout components and added a CSS module for the header with a test style

---
+++ src/components/header.js
@@ -0,0 +1,8 @@
+import React from "react"
+import headerStyles from "../styles/header.module.css"
+
+const Header = () => {
+  return <div className={headerStyles.header}>Header</div>
+}
+
+export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
 import React from "react"
+import Layout from "../components/layout"
+import Header from "../components/header"
 
-export default function Home() {
-  return <div>Hello world!</div>
+const Home = () => {
+  return (
+    <Layout>
+      <Header />
+      <p>Hello World</p>
+    </Layout>
+  )
 }
+
+export default Home
