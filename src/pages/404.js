4661802180296213865d721a83c3951447404d8e
ren1el <renielxocampo@gmail.com>
2020-09-23 15:04:09 -0700

    Added helmet to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,19 @@
 import React from "react"
+import { Helmet } from "react-helmet"
+import config from "../config/index"
 import pageNotFoundStyles from "../styles/404.module.css"
 import Layout from "../components/layout"
 
-const Home = () => {
+const NotFound = () => {
+  const { siteIcon } = config
+
   return (
     <Layout>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <link rel="icon" href={siteIcon} />
+        <title>404: Not Found</title>
+      </Helmet>
       <section className={pageNotFoundStyles.wrapper}>
         <h1 className={pageNotFoundStyles.pageNotFound}>404</h1>
         <p>
@@ -17,4 +26,4 @@ const Home = () => {
   )
 }
 
-export default Home
+export default NotFound
