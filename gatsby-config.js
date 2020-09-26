369239e965e82a86abb9f21b2a0c875f1d9112ad
ren1el <renielxocampo@gmail.com>
2020-09-26 13:10:11 -0700

    Added manifest plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,5 +18,18 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "GatsbyJS",
+        short_name: "GatsbyJS",
+        start_url: "/",
+        background_color: "#6b37bf",
+        theme_color: "#6b37bf",
+        display: "standalone",
+        icon: "static/favicon.png",
+        crossOrigin: `use-credentials`,
+      },
+    },
   ],
 }
